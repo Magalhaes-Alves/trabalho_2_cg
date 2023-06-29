558c3e36c4b63189600de94cff47e7d5004f66e0
gabriel magalhaes <magalhaesgabriel124@gmail.com>
2023-06-29 16:35:40 -0300

    Inicio Textura

--- src/webgl.js
+++ src/webgl.js
@@ -42,74 +42,84 @@ function createProgram(gl, vtxShader, fragShader)
 
 function init()
 {
+    var teximg = new Image();
+    teximg.src = "img/gato.jpg"
 
-	    var canvas = document.getElementById("cg");
-	
-	    var gl = getGL(canvas);
-	    if(gl)
-	    {
-            //Inicializa shaders
- 		    var vtxShSrc = document.getElementById("vertex-shader").text;
-		    var fragShSrc = document.getElementById("frag-shader").text;
+    var canvas = document.getElementById("cg");
 
-            var vtxShader = createShader(gl, gl.VERTEX_SHADER, vtxShSrc);
-            var fragShader = createShader(gl, gl.FRAGMENT_SHADER, fragShSrc);
-            var prog = createProgram(gl, vtxShader, fragShader);	
-            
-            //Avisa ao webgl que deve usar o seguinte programa
-            gl.useProgram(prog);
+    var gl = getGL(canvas);
+    if(gl)
+    {
+        //Inicializa shaders
+        var vtxShSrc = document.getElementById("vertex-shader").text;
+        var fragShSrc = document.getElementById("frag-shader").text;
 
-            //Inicializa Ã¡rea de desenho: viewport e cor de limpeza; limpa a tela
-            gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-            gl.clearColor(0, 0, 0, 1);
-            gl.enable( gl.BLEND );
-            gl.blendFunc( gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA );
-
-        }
-        //Define coordenadas dos triÃ¢ngulos
-        var coordTriangles = new Float32Array([
-        									  -0.5,  -0.5, 1.0, 0.0, 0.0, 1.0, 
-        									   0.5,  -0.5, 0.0, 1.0, 0.0, 1.0, 
-        									   0.0,   1.0, 0.0, 0.0, 1.0, 1.0,
-        									  -1.0,   0.0, 1.0, 1.0, 0.0, 0.7,
-        									   1.0,   0.0, 1.0, 0.0, 1.0, 0.7, 
-        									   0.0,   0.5, 0.0, 1.0, 1.0, 0.7
-        									 ]);
-        									
-        //Cria buffer na GPU e copia coordenadas para ele
-        var bufPtr = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, bufPtr);
-        gl.bufferData(gl.ARRAY_BUFFER, coordTriangles, gl.STATIC_DRAW);
+        var vtxShader = createShader(gl, gl.VERTEX_SHADER, vtxShSrc);
+        var fragShader = createShader(gl, gl.FRAGMENT_SHADER, fragShSrc);
+        var prog = createProgram(gl, vtxShader, fragShader);	
         
-        //Pega ponteiro para o atributo "position" do vertex shader
-        var positionPtr = gl.getAttribLocation(prog, "position");
-        gl.enableVertexAttribArray(positionPtr);
-        //Especifica a cÃ³pia dos valores do buffer para o atributo
-        gl.vertexAttribPointer(positionPtr, 
-        					   2,        //quantidade de dados em cada processamento
-        					   gl.FLOAT, //tipo de cada dado (tamanho)
-        					   false,    //nÃ£o normalizar
-        					   6*4,      //tamanho do bloco de dados a processar em cada passo
-        					             //0 indica que o tamanho do bloco Ã© igual a tamanho
-        					             //lido (2 floats, ou seja, 2*4 bytes = 8 bytes)
-        					   0         //salto inicial (em bytes)
-        					  );
-        
-        var colorPtr = gl.getAttribLocation(prog, "fcolor");
-        gl.enableVertexAttribArray(colorPtr);
-        //Especifica a cÃ³pia dos valores do buffer para o atributo
-        gl.vertexAttribPointer(colorPtr, 
-        					   4,        //quantidade de dados em cada processamento
-        					   gl.FLOAT, //tipo de cada dado (tamanho)
-        					   false,    //nÃ£o normalizar
-        					   6*4,      //tamanho do bloco de dados a processar em cada passo
-        					             //0 indica que o tamanho do bloco Ã© igual a tamanho
-        					             //lido (2 floats, ou seja, 2*4 bytes = 8 bytes)
-        					   2*4       //salto inicial (em bytes)
-        					  );
+        //Avisa ao webgl que deve usar o seguinte programa
+        gl.useProgram(prog);
 
-        
-        gl.clear( gl.COLOR_BUFFER_BIT);
-        
-        gl.drawArrays(gl.TRIANGLES, 0, 6);
+        //Inicializa Ã¡rea de desenho: viewport e cor de limpeza; limpa a tela
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+        gl.clearColor(0, 0, 0, 1);
+        gl.enable( gl.BLEND );
+        gl.blendFunc( gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA );
+
+    }
+    //Define coordenadas dos triÃ¢ngulos
+    /*var coordTriangles = new Float32Array([
+                                            -0.5,  0.5, 1.0, 1.0, 0.0, 1.0, 
+                                            -0.5, -0.5, 1.0, 0.0, 0.0, 1.0, 
+                                            0.5, -0.5, 1.0, 1.0, 0.0, 1.0,
+                                            0.5,  0.5, 1.0, 0.0, 0.0, 1.0, 
+                                            -0.5, 0.5, 1.0, 1.0, 0.0, 1.0
+                                            ]);
+    */									
+    
+    var coordTriangles = new Float32Array([
+                                            -0.5,  0.5, 0.0, 0.0, 
+                                            -0.5, -0.5, 0.0, 1.0,
+                                                0.5, -0.5, 1.0, 1.0, 
+                                                0.5,  0.5, 1.0, 0.0, 
+                                                -0.5, 0.5, 0.0, 0.0
+                                            ]);
+    //Cria buffer na GPU e copia coordenadas para ele
+    var bufPtr = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufPtr);
+    gl.bufferData(gl.ARRAY_BUFFER, coordTriangles, gl.STATIC_DRAW);
+    
+    //Pega ponteiro para o atributo "position" do vertex shader
+    var positionPtr = gl.getAttribLocation(prog, "position");
+    gl.enableVertexAttribArray(positionPtr);
+    //Especifica a cÃ³pia dos valores do buffer para o atributo
+    gl.vertexAttribPointer(positionPtr, 
+                            2,        //quantidade de dados em cada processamento
+                            gl.FLOAT, //tipo de cada dado (tamanho)
+                            false,    //nÃ£o normalizar
+                            4*4,      //tamanho do bloco de dados a processar em cada passo
+                                        //0 indica que o tamanho do bloco Ã© igual a tamanho
+                                        //lido (2 floats, ou seja, 2*4 bytes = 8 bytes)
+                            0         //salto inicial (em bytes)
+                            );
+    
+    var textCoordPtr = gl.getAttribLocation(prog, "ftextCoord");
+    gl.enableVertexAttribArray(textCoordPtr);
+    //Especifica a cÃ³pia dos valores do buffer para o atributo
+    gl.vertexAttribPointer(textCoordPtr, 
+                            2,        //quantidade de dados em cada processamento
+                            gl.FLOAT, //tipo de cada dado (tamanho)
+                            false,    //nÃ£o normalizar
+                            4*4,      //tamanho do bloco de dados a processar em cada passo
+                                        //0 indica que o tamanho do bloco Ã© igual a tamanho
+                                        //lido (2 floats, ou seja, 2*4 bytes = 8 bytes)
+                            2*4       //salto inicial (em bytes)
+                            );
+
+    
+    gl.clear( gl.COLOR_BUFFER_BIT);
+    
+    gl.drawArrays(gl.TRIANGLES, 0,3);
+    gl.drawArrays(gl.TRIANGLES,2,3);
 }
